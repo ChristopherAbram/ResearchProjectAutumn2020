8cbe968643068799d3a93273a9dd0b1285718d0e
Krzysztof Abram <krza@itu.dk>
2020-10-09 16:23:04 +0200

    + Correct implementation of utils.raster.RasterWindow and *.RasterWindowSize + Update examples and add some description

--- src/examples/fb_by_window_example.py
+++ src/examples/fb_by_window_example.py
@@ -6,7 +6,7 @@ import itertools
 from matplotlib.colors import LogNorm
 
 from utils.definitions import get_project_path
-from utils.raster import RasterWindow
+from utils.raster import RasterWindow, RasterWindowSize
 
 
 def main(argc, argv):
@@ -16,10 +16,20 @@ def main(argc, argv):
         get_project_path(), "data", "humdata", '%s' % country, 'population_%s_2018-10-01.tif' % country.lower())
 
     cmap = plt.cm.magma
-    for window in RasterWindow(in_file, 100, 100):
+
+    # Read by window, define horizontal and vertical split.
+    # e.g. RasterWindow(in_file, 8, 4) splits underlying data into matrix of 8 columns and 4 rows.
+    for (window, (row, col), (width, height)) in RasterWindow(in_file, 8, 4):
         fig, ax = plt.subplots()
         ax.imshow(window, cmap=cmap, norm=LogNorm())
         plt.show()
+
+    # Read by window size, 5000 X 3000 px
+    # Note: The sizes around edge will differ from given size. You can get these values from (width, height)
+    # for (window, (row, col), (width, height)) in RasterWindowSize(in_file, 5000, 3000):
+    #     fig, ax = plt.subplots()
+    #     ax.imshow(window, cmap=cmap, norm=LogNorm())
+    #     plt.show()
 
     return 0
 
--- src/utils/raster.py
+++ src/utils/raster.py
@@ -11,26 +11,47 @@ class RasterWindow:
         self.filepath = filepath
         self.num_xslices = x_slices
         self.num_yslices = y_slices
-
-        self.__raster = rasterio.open(self.filepath)
-
+        self._raster = rasterio.open(self.filepath)
 
     def __iter__(self):
-        self.__w_pairs = itertools.product(
-            range(self.num_xslices), 
-            range(self.num_yslices))
+        self._w_pairs = itertools.product(
+            range(self.num_yslices),
+            range(self.num_xslices))
         return self
-
 
     def __next__(self):
         try:
-            xi, yi = next(self.__w_pairs)
+            xi, yi = next(self._w_pairs)
             si_width, si_height = \
-                int(self.__raster.width / self.num_xslices), \
-                int(self.__raster.height / self.num_yslices)
+                int(self._raster.width / self.num_xslices), \
+                int(self._raster.height / self.num_yslices)
             w = Window.from_slices(
-                (yi * si_height, yi * si_height + si_height), 
-                (xi * si_width, xi * si_width + si_width))
-            return self.__raster.read(1, window=w)
+                (xi * si_height, xi * si_height + si_height), 
+                (yi * si_width, yi * si_width + si_width))
+            X = self._raster.read(1, window=w)
+            return X, (xi, yi), (X.shape[1], X.shape[0])
         except StopIteration:
+            self.__raster.close()
+            raise
+
+
+class RasterWindowSize(RasterWindow):
+    def __init__(self, filepath, width=1024, height=1024):
+        super(RasterWindowSize, self).__init__(filepath)
+        self.width = width
+        self.height = height
+        self.num_xslices = int(np.ceil(self._raster.width / self.width))
+        self.num_yslices = int(np.ceil(self._raster.height / self.height))
+
+    def __next__(self):
+        try:
+            xi, yi = next(self._w_pairs)
+            si_width, si_height = self.width, self.height
+            w = Window.from_slices(
+                (xi * si_height, xi * si_height + si_height), 
+                (yi * si_width, yi * si_width + si_width))
+            X = self._raster.read(1, window=w)
+            return X, (xi, yi), (X.shape[1], X.shape[0])
+        except StopIteration:
+            self._raster.close()
             raise
